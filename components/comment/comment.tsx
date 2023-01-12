63b4d512ecec6e9f28552853b27c350b50b3fa6a
shako0 <chxetiani.95@gmail.com>
2023-01-12 15:57:58 +0400

    comment

--- components/comment/comment.tsx
+++ components/comment/comment.tsx
@@ -9,7 +9,7 @@ interface PropsType {
 
 const YourComment: FC<PropsType> = ({ message, deleteComment }) => {
   return (
-    <section className="w-full bg-slate-800 h-[calc(100vh-96px)]">
+    <section className="w-full bg-slate-800  min-h-[calc(100vh-96px)]">
       <h2 className="text-[30px] text-white text-center font-bold pt-[15px]">
         Comments
       </h2>
@@ -33,28 +33,23 @@ const YourComment: FC<PropsType> = ({ message, deleteComment }) => {
       )}
       <div className=" md:w-[65%] maxmd:mx-3 m-auto rounded-md flex flex-col justify-center items-center">
         {message.map((item, index) => (
-          <div
-            key={item._id}
-            className="bg-slate-300 px-3 py-3 w-[70%] pb-6 m-5 rounded-md"
-          >
-            <div className="text-black lg:flex m-5 rounded-lg overflow-auto">
-              <div className="md:min-w-[120px] max-w-[120px] p-2 font-bold">
+          <div key={item._id} className="bg-slate-300 p-3 w-[70%] rounded-md mb-10 maxmd:px-5">
+            <div className="text-black lg:flex rounded-lg">
+              <div className="md:min-w-[120px] max-w-[120px] p-2 pl-3 font-bold">
                 {item.name}
               </div>
-              <div className="p-2">
-                <h4 className="mb-2 italic text-blue-500">{item.email}.</h4>
-                <p className="">{item.message}</p>
-              </div>
-            </div>
-            <div className="grid grid-rows gap-3 md:grid md:grid-cols-2 ">
-              <div className="text-center">
-                <button
-                  className=" bg-blue-400 text-black text-[13px] p-2 rounded-md font-bold 
-                hover:bg-slate-500 hover:text-white active:bg-slate-400 active:text-black"
-                  onClick={() => deleteComment(index)}
-                >
-                  delete comment
-                </button>
+              <div className="p-3 w-full">
+                <h4 className="mb-5 italic text-blue-600">{item.email}.</h4>
+                <p className="bg-slate-200 p-3 rounded-[6px] overflow-auto">{item.message}</p>
+                <div className="text-end">
+                  <button
+                    className="mt-8 bg-blue-600 text-white text-[13px] p-2 rounded-md font-bold 
+          hover:bg-slate-600"
+                    onClick={() => deleteComment(index)}
+                  >
+                    delete comment
+                  </button>
+                </div>
               </div>
             </div>
           </div>
